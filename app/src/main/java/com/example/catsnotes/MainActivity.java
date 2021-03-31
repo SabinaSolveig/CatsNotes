8f94ef98b6bd439deffad99ffb6903256057d3ec
SabinaSolveig <sabinasolveig@gmail.com>
2021-03-31 20:30:44 +0400

    Initial commit

---
+++ app/src/main/java/com/example/catsnotes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.catsnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
